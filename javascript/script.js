b38cc47324be26b26eae1c59b75a079762c68db0
Mariano Giagante <mariano.giagante@gmail.com>
2017-06-05 22:50:07 -0300

    Extracts matrix creation to its own function; Calls it after clicking reset button with the size chosen by the user

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,27 @@
-$(document).ready(function() {
-  for (var i = 1; i <= 16; i++) {
+function initializeMatrix(matrixSize) {
+  $('#container').empty(); // Removes any pre-existing cells
 
-    $('#container').append('<div class="cell clear-left"></div>');
+  for (var i = 1; i <= matrixSize; i++) {
 
-    for (var j = 2; j <= 16; j++) {
+    $('#container').append('<div class="cell clear-left"></div>'); // First cell clears at left
+
+    for (var j = 2; j <= matrixSize; j++) {
       $('#container').append('<div class="cell"></div>');
     }
 
   }
 
-  $('div').mouseenter(function() {
+  $('.cell').mouseenter(function() { // Makes cells become painted when mouse enters to them
     $(this).addClass('painted');
   });
+}
+
+$(document).ready(function() {
+  initializeMatrix(16); // Initial matrix is 16x16
 
   $('button#reset').click(function() {
     $('.cell').removeClass('painted');
+    var newSize = prompt('What size do you want now?');
+    initializeMatrix(newSize);
   });
 });
